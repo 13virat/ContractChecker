19bc5008c9984c0a6b607172687f28551cd88de9
Virat Gupta <114520937+13virat@users.noreply.github.com>
2026-01-17 14:16:24 +0530

    Update wsgi.py

--- contract_checker/contract_checker/wsgi.py
+++ contract_checker/contract_checker/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for contract_checker project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/ZenBackend/ContractChecker'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'contract_checker.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contract_checker.settings')
-
 application = get_wsgi_application()
